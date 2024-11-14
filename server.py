b5bc45f63d0bde0eb9c5f18e502fe8238e9a3653
Mohamed Amine Fakhre-Eddine <mohamedfakhreeddine2019@gmail.com>
2024-11-14 18:01:43 +0100

    Update server.py

--- server.py
+++ server.py
@@ -36,4 +36,4 @@ def calculate():
         return jsonify({'error': 'Invalid numbers'}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
